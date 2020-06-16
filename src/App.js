75c892dcc135d2c25659b44ae3f273388ac053d0
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-06-15 22:01:20 -0500

    style: Styled Components for the Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 import Header from './componets/header'
 import Board from './componets/board'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
       <Board/>
-    </div>
+    </>
   );
 }
 
--- src/componets/header.js
+++ src/componets/header.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 import logo from './../assets/logo.svg'
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 10px 1px;
+  height: 50px;
+  padding: 10px;
+  .header-logo {
+    height: 50px;
+    width: 50px;
+  }
+  .header-title {
+    color: white;
+    font-size: 1.1em;
+    margin: 0;
+  }
+  @media (prefers-reduced-motion: no-preference) {
+    .header-logo {
+      animation: header-logo-spin infinite 20s linear;
+    }
+  }
+  @keyframes header-logo-spin {
+    from {transform: rotate(0deg);}
+    to {transform: rotate(360deg);}
+  }
+` 
 
 function Header(){
   return(
-    <header>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Challenge from React.</p>
-    </header>
+    <StyledHeader>
+      <img src={logo} className="header-logo" alt="logo" />
+      <h1 className="header-title">Challenge from React.</h1>
+    </StyledHeader>
   )
 }
 
