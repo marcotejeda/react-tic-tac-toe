7eeec616e0f0eff1bfbda143de1c85567a4bd7dd
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-06-15 22:10:46 -0500

    Updates

---
+++ precache-manifest.8bb8bcaee785e4c2a6df6cd21ddb269e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19baa15bbbab6c3873e1f68862b14665",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "92e8373a0a722bfa5f7d",
+    "url": "/react-tic-tac-toe/static/css/main.1c46a8b5.chunk.css"
+  },
+  {
+    "revision": "8f9cc2ad7df262641abc",
+    "url": "/react-tic-tac-toe/static/js/2.1cacdd51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tic-tac-toe/static/js/2.1cacdd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e8373a0a722bfa5f7d",
+    "url": "/react-tic-tac-toe/static/js/main.42de0231.chunk.js"
+  },
+  {
+    "revision": "b7eef19b3de598493726",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.6a378528.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9525d92f3c2104d596b2c68c34e03c00.js"
+  "/react-tic-tac-toe/precache-manifest.8bb8bcaee785e4c2a6df6cd21ddb269e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
