e8d9edc13a18d5787e0347bebafb70c230dfa1b1
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-06-18 23:35:56 -0500

    style: Styled Components for the Square component

--- src/componets/square.js
+++ src/componets/square.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function Square() {
+const Button = styled.button`
+  background: ${props => props.win ? "#282c34" : "white"};
+  border: 2px solid #282c34;
+  color: ${props => props.win ? "white" : "#282c34"};
+  font-size: 1.8em;
+  font-weight: ${props => props.win ? "600" : "100"};
+  line-height: 50px;
+  height: 50px;
+  margin-right: -2px;
+  margin-top: -2px;
+  padding: 0;
+  text-align: center;
+  width: 50px;
+  :focus {
+    outline: none;
+  }
+`
+
+function Square(props) {
   return(
-    <div>
-      Squere
-    </div>
+    <Button>
+      X
+    </Button>
   )
 }
 
