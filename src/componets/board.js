dc33d4a8bb1091f373e2a6309e00de076d0e6ccd
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-06-18 23:50:11 -0500

    style: Styled Components for the Board component

--- src/componets/board.js
+++ src/componets/board.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Square from './square'
+import styled from 'styled-components'
+
+const StyledBoard = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  margin: 5em auto 0;
+  width: 144px;
+`
 
 function Board() {
   return(
-    <div>
-      Board
+    <StyledBoard>
       <Square />
-    </div>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </StyledBoard>
   )
 }
 
