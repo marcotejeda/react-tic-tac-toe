292196755f6f08bf024b3a949ccd87e51d0dbb9b
Marco Tejeda <jeanmarcotejeda@gmail.com>
2020-07-19 00:28:50 -0500

    Updates

---
+++ precache-manifest.2e21da0e0ac4dfea0a54a534cbf393ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d371ba8d10c3b29a8a00b2b6a7ee357",
+    "url": "/React-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "abd102d24c94288582b7",
+    "url": "/React-tic-tac-toe/static/css/main.f018ceec.chunk.css"
+  },
+  {
+    "revision": "0ce00ab0f565c05a242f",
+    "url": "/React-tic-tac-toe/static/js/2.832363a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-tic-tac-toe/static/js/2.832363a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd102d24c94288582b7",
+    "url": "/React-tic-tac-toe/static/js/main.6f06db5f.chunk.js"
+  },
+  {
+    "revision": "84894c8a3222808a172d",
+    "url": "/React-tic-tac-toe/static/js/runtime-main.bb600247.js"
+  },
+  {
+    "revision": "08d0b185d09093fd90edbf189732552e",
+    "url": "/React-tic-tac-toe/static/media/arrow-down.08d0b185.svg"
+  },
+  {
+    "revision": "4d95a11f8222a956ecc4a9f4eac2f2b2",
+    "url": "/React-tic-tac-toe/static/media/arrow-top.4d95a11f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tic-tac-toe/precache-manifest.ab7264b45745b1b2cadeeadc822fe4d3.js"
+  "/React-tic-tac-toe/precache-manifest.2e21da0e0ac4dfea0a54a534cbf393ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tic-tac-toe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
